ccbeaa4e4ad0c061ae3428a166f0ce8c4489a89c
DevPGSV <DevPGSV@gmail.com>
2019-04-22 15:24:07 +0200

    Set charset to UTF8 to be able to display special chars

--- js/script.js
+++ js/script.js
@@ -897,7 +897,7 @@ var PACMAN = (function () {
         ctx.fillStyle = !soundDisabled() ? "#00FF00" : "#FF0000"; //custom: enabled-disabled color
         ctx.font = "bold 16px sans-serif";
         ctx.fillText("♪", 10, textBase);
-       // ctx.fillText("s", 10, textBase);
+        //ctx.fillText("s", 10, textBase);
 
         ctx.fillStyle = "#FFFFFF"; //custom: footer words
         ctx.font      = "14px Calibri";
